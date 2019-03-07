d23973679f2588e7c7a9f412e632c1ea8f645c4c
shimingzhao <34359110+shimingzhao@users.noreply.github.com>
2019-03-06 22:18:07 -0500

    frostedglass css style

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
+        {/* <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
             Edit <code>src/App.js</code> and save to reload.
@@ -19,7 +19,13 @@ class App extends Component {
           >
             Learn React
           </a>
-        </header>
+        </header> */}
+        <div className="frostedglass">
+          <div className="frostedglass-inner">前端开发</div>
+        </div> 
+        <div className="frostedglass">
+          <div className="frostedglass-inner">HTML</div>
+        </div>
       </div>
     );
   }
