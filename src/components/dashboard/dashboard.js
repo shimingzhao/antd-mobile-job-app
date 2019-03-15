02e9685dfb5057ee08e0f2b0ea23ef01dc57d9bf
shimingzhao <34359110+shimingzhao@users.noreply.github.com>
2019-03-15 00:29:04 -0400

    dashboard, TabBar, NavLinkBar

---
+++ src/components/dashboard/dashboard.js
@@ -0,0 +1,73 @@
+import React, { Component } from 'react'
+import {connect} from 'react-redux'
+import {Route} from 'react-router-dom'
+import {TabBar, Icon, NavBar} from 'antd-mobile'
+import NavLinkBar from '../navlink/navlink'
+// import Boss from './'
+
+function Boss(){
+  return <h3>Boss Page</h3>
+}
+
+function Genius(){
+  return <h3>Genius Page</h3>
+}
+
+function Msg(){
+  return <h3>Message page</h3>
+}
+
+function User(){
+  return <h3>My page</h3>
+}
+
+@connect(
+  state => state
+)
+class Dashboard extends Component {
+  render() {
+    const user = this.props.user
+    const {pathname} = this.props.location
+    const navList = [
+      {
+        path: '/boss',
+        text: 'Genius',
+        icon: 'boss',
+        title: 'Genius List',
+        component: Boss,
+        hide: user.type === 'genius'
+      },
+      {
+        path: '/genius',
+        text: 'Jobs',
+        icon: 'job',
+        title: 'Job List',
+        component: Genius,
+        hide: user.type === 'boss'
+      },
+      {
+        path: '/msg',
+        text: 'Message',
+        icon: 'msg',
+        title: 'Message List',
+        component: Msg
+      },
+      {
+        path: '/me',
+        text: 'My Profile',
+        icon: 'user',
+        title: 'My Profile',
+        component: User
+      }
+    ]
+    return (
+      <div>
+        <NavBar mode="dark">{navList.find(v=>v.path===pathname).title}</NavBar>
+        <h2>content</h2>
+        <NavLinkBar data={navList}></NavLinkBar>
+      </div>
+    )
+  }
+}
+
+export default Dashboard
--- src/index.js
+++ src/index.js
@@ -15,24 +15,26 @@ import Register from './container/register/register'
 import AuthRoute from './components/authroute/authroute'
 import BossInfo from './container/bossinfo/bossinfo'
 import GeniusInfo from './container/geniusinfo/geniusinfo'
+import Dashboard from './components/dashboard/dashboard'
 
 const store = createStore(reducers, compose(
     applyMiddleware(thunk),
     window.__REDUX_DEVTOOLS_EXTENSION__() ? window.__REDUX_DEVTOOLS_EXTENSION__() : f => f
 ))
 
-const Boss = () => <h2>Boss page</h2>
-
-
+// boss genius me msg 4 pages
 ReactDOM.render((
     <Provider store={store}>
         <BrowserRouter>
             <div>
                 <AuthRoute />
-                <Route path='/bossinfo' component={BossInfo}/>
-                <Route path='/geniusinfo' component={GeniusInfo}/>
-                <Route path="/login" component={Login} />
-                <Route path="/register" component={Register} />
+                <Switch>
+                    <Route path='/bossinfo' component={BossInfo}/>
+                    <Route path='/geniusinfo' component={GeniusInfo}/>
+                    <Route path="/login" component={Login} />
+                    <Route path="/register" component={Register} />
+                    <Route component={Dashboard} />
+                </Switch>
             </div>
         </BrowserRouter>
     </Provider>), document.getElementById('root'))
