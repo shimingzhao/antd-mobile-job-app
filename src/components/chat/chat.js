240aae0e375dcc4037f3e8c8a6d2666bbd946853
shimingzhao <34359110+shimingzhao@users.noreply.github.com>
2019-05-10 01:04:27 -0400

    chat - redux

--- src/components/chat/chat.js
+++ src/components/chat/chat.js
@@ -1,32 +1,45 @@
 import React, { Component } from 'react';
-import io from 'socket.io-client';
 import { List, InputItem } from 'antd-mobile';
+import { connect } from 'react-redux';
+import { getMsgList } from '../../redux/chat.redux';
+import io from 'socket.io-client';
 const socket = io('ws://localhost:9093');
-socket.on('recvmsg', function(data) {
-  console.log(data);
-});
 
+@connect(
+  state => state,
+  { getMsgList }
+)
 class Chat extends Component {
   constructor(props) {
     super(props);
-    this.state = { text: '' };
+    this.state = { text: '', msg: [] };
   }
-  componentDidMount() {}
+  componentDidMount() {
+    this.props.getMsgList();
+    // socket.on('recvmsg', data => {
+    //   this.setState({ msg: [...this.state.msg, data.text] });
+    // });
+  }
   handleSubmit() {
-    socket.emit('sendmsg', { text: this.state.text });
+    // socket.emit('sendmsg', { text: this.state.text });
     this.setState({ text: '' });
   }
   render() {
     return (
-      <div className="stick-footer">
-        <List>
-          <InputItem
-            placeholder="Enter the message"
-            value={this.state.text}
-            onChange={v => this.setState({ text: v })}
-            extra={<span onClick={() => this.handleSubmit()}>Send</span>}
-          />
-        </List>
+      <div>
+        {this.state.msg.map(v => {
+          return <p key={v}>{v}</p>;
+        })}
+        <div className="stick-footer">
+          <List>
+            <InputItem
+              placeholder="Enter the message"
+              value={this.state.text}
+              onChange={v => this.setState({ text: v })}
+              extra={<span onClick={() => this.handleSubmit()}>Send</span>}
+            />
+          </List>
+        </div>
       </div>
     );
   }
