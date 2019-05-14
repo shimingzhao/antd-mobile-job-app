181f4e378a1cc1c2291474482e7812f90776b0fd
shimingzhao <34359110+shimingzhao@users.noreply.github.com>
2019-05-13 23:41:06 -0400

    chat

--- src/components/chat/chat.js
+++ src/components/chat/chat.js
@@ -1,13 +1,13 @@
 import React, { Component } from 'react';
-import { List, InputItem } from 'antd-mobile';
+import { List, InputItem, NavBar } from 'antd-mobile';
 import { connect } from 'react-redux';
-import { getMsgList } from '../../redux/chat.redux';
+import { sendMsg } from '../../redux/chat.redux';
 import io from 'socket.io-client';
 const socket = io('ws://localhost:9093');
 
 @connect(
   state => state,
-  { getMsgList }
+  { sendMsg }
 )
 class Chat extends Component {
   constructor(props) {
@@ -15,20 +15,38 @@ class Chat extends Component {
     this.state = { text: '', msg: [] };
   }
   componentDidMount() {
-    this.props.getMsgList();
     // socket.on('recvmsg', data => {
     //   this.setState({ msg: [...this.state.msg, data.text] });
     // });
   }
   handleSubmit() {
     // socket.emit('sendmsg', { text: this.state.text });
+    // this.setState({ text: '' });
+    const from = this.props.user._id;
+    const to = this.props.match.params.user;
+    const msg = this.state.text;
+    this.props.sendMsg({ from, to, msg });
     this.setState({ text: '' });
   }
   render() {
+    const user = this.props.match.params.user;
+    const Item = List.Item;
     return (
-      <div>
-        {this.state.msg.map(v => {
-          return <p key={v}>{v}</p>;
+      <div id="chat-page">
+        <NavBar mode="dark">{this.props.match.params.user}</NavBar>
+
+        {this.props.chat.chatmsg.map(v => {
+          return v.from === user ? (
+            <List key={v._id}>
+              <Item>{v.content}</Item>
+            </List>
+          ) : (
+            <List key={v._id}>
+              <Item className="chat-me" extra={'avatar'}>
+                {v.content}
+              </Item>
+            </List>
+          );
         })}
         <div className="stick-footer">
           <List>
--- src/redux/chat.redux.js
+++ src/redux/chat.redux.js
@@ -24,7 +24,12 @@ export function chat(state = initState, action) {
         chatmsg: action.payload,
         unread: action.payload.filter(v => !v.read).length
       };
-    // case MSG_RECV:
+    case MSG_RECV:
+      return {
+        ...state,
+        chatmsg: [...state.chatmsg, action.payload],
+        unread: state.unread + 1
+      };
     // case MSG_READ:
     default:
       return state;
@@ -32,13 +37,32 @@ export function chat(state = initState, action) {
 }
 
 function msgList(msgs) {
-  return { type: 'MSG_LIST', payload: msgs };
+  return { type: MSG_LIST, payload: msgs };
+}
+
+function msgRecv(msg) {
+  return { type: MSG_RECV, payload: msg };
+}
+
+export function recvMsg() {
+  return dispatch => {
+    socket.on('recvmsg', function(data) {
+      console.log('recvmsg', data);
+      dispatch(msgRecv(data));
+    });
+  };
+}
+
+export function sendMsg({ from, to, msg }) {
+  return dispatch => {
+    socket.emit('sendmsg', { from, to, msg });
+  };
 }
 
 export function getMsgList() {
   return dispatch => {
     axios.get('/user/getmsglist').then(res => {
-      if (res.state === 200 && res.data.code === 0) {
+      if (res.status === 200 && res.data.code === 0) {
         dispatch(msgList(res.data.msgs));
       }
     });
