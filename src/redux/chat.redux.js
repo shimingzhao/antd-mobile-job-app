e9713d948c1c9c6a70de08b5c0b986f3ae7fe052
shimingzhao <34359110+shimingzhao@users.noreply.github.com>
2019-05-29 01:05:13 -0400

    unread message number

--- src/redux/chat.redux.js
+++ src/redux/chat.redux.js
@@ -28,22 +28,20 @@ export function chat(state = initState, action) {
       };
     case MSG_RECV:
       const n = action.payload.to === action.userid ? 1 : 0;
-      
       return {
         ...state,
         chatmsg: [...state.chatmsg, action.payload],
         unread: state.unread + n
       };
     case MSG_READ:
-      const { user, chatwith } = action;
-      const chatid = [user, chatwith].sort().join('_');
+      const { from, userid, num } = action.payload;
       return {
         ...state,
-        msgs: state.msgs.map(v => {
-          if (v.chatid === chatid && user === v.to) {
-            v.read = true;
-          }
-        })
+        chatmsg: state.chatmsg.map(v => ({
+          ...v,
+          read: from === v.from ? true : v.read
+        })),
+        unread: state.unread - num
       };
     default:
       return state;
@@ -58,10 +56,24 @@ function msgRecv(msg, userid) {
   return { userid, type: MSG_RECV, payload: msg };
 }
 
+function msgRead({ from, userid, num }) {
+  return { type: MSG_READ, payload: { from, userid, num } };
+}
+
+export function readMsg(from) {
+  return (dispatch, getState) => {
+    axios.post('/user/readmsg', { from }).then(res => {
+      const userid = getState().user._id;
+      if (res.status === 200 && res.data.code === 0) {
+        dispatch(msgRead({ from, userid, num: res.data.num }));
+      }
+    });
+  };
+}
+
 export function recvMsg() {
   return (dispatch, getState) => {
     socket.on('recvmsg', function(data) {
-      console.log('recvmsg', data);
       const userid = getState().user._id;
       dispatch(msgRecv(data, userid));
     });
